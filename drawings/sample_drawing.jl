75eb5f60ad909770fd34f2b9b771b83a9f36b920
Skylar A Gering <skygering@gmail.com>
2023-06-27 16:19:40 -0700

    Add sample drawing file and folder

---
+++ drawings/sample_drawing.jl
@@ -0,0 +1,8 @@
+# Find emojis at https://docs.julialang.org/en/v1/manual/unicode-input/
+# Fill in your drawing here. Delete the outline and comments when you are done.
+
+#  ---------------------------
+# |                           |
+# |                           |
+# |                           |
+#  ---------------------------
