9ec32291beb3b2f41669d8a8f0ba5fd0e64cafb1
Skylar A Gering <skygering@gmail.com>
2023-06-29 11:46:55 -0700

    Update drawing instructions

--- drawings/sample_drawing.jl
+++ drawings/sample_drawing.jl
@@ -1,5 +1,5 @@
-# Find emojis at https://docs.julialang.org/en/v1/manual/unicode-input/
-# Fill in your drawing here. Delete the outline and comments when you are done.
+# Use the emojis in the emoji bank to make a drawing. Fill in lines 5-7.
+# Leave the line start comments, but you can remove the outline if you want.
 
 #  ---------------------------
 # |                           |
